cecdd34fba13886adb28372d72465ae8a36a5f42
CristianoChiaramelli <cristianochiaramelli@gmail.com>
2016-12-08 14:45:30 -0200

    Coloquei uma entrada nova, pq nao tinha entendido a outra, alem de nao estar compilando...

--- wordwrap.c
+++ wordwrap.c
@@ -1,25 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
 #define MIN(a,b) (((a)<(b))?(a):(b))
 
-
-int getInput(char ***vector, int *lineSize){
-	int n;
+char **getInput(int *n, int *lineSize){
+	int i;
 	ssize_t wordSize;
-	scanf("%d", &n);
+	scanf("%d", n);
 	scanf("%d", lineSize);
+
+	char **vector = (char**) malloc(sizeof(char*) * (*n));
+
 	getchar();
-	*vector = malloc(sizeof(char**)*(n+1));
-       	while(n > 0){
-		getline(*vector+n, &wordSize, stdin);
-		n--;
-	}	
+    for (i=0; i<*n; i++){
+    	vector[i] = (char*) malloc(sizeof(char)*(*lineSize+1));
+		int pos = getline((&vector[i]), &wordSize, stdin);
+	}
+	return vector;
+}
+
+void printVector(char **vector, int n){
+	int i;
+	for (i=0; i<n; i++){
+		printf ("%s", vector[i]);
+	}
 }
 
 int main(int argc, char **argv){
-	int n, &lineSize;
-	char **vector = NULL;
-	n = getInput(&vector, &lineSize);
-	printf("%d\n", opt(vector, n);
+	int n, lineSize;
+	char **vector = getInput(&n, &lineSize);
+
+	printVector(vector, n);
 }
