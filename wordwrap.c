182ff18b31b808b29e90cee6d9f9c6fb93a8f388
CristianoChiaramelli <cristianochiaramelli@gmail.com>
2016-12-09 16:48:59 -0200

    Final

--- wordwrap.c
+++ wordwrap.c
@@ -8,6 +8,7 @@
 char **getInput(int *n, int *lineSize){
 	int i;
 	ssize_t wordSize;
+
 	scanf("%d", n);
 	scanf("%d", lineSize);
 
@@ -17,6 +18,8 @@ char **getInput(int *n, int *lineSize){
     for (i=0; i<*n; i++){
     	vector[i] = (char*) malloc(sizeof(char)*(*lineSize+1));
 		int pos = getline((&vector[i]), &wordSize, stdin);
+
+		vector[i][pos-1] = '\0';
 	}
 	return vector;
 }
@@ -24,78 +27,53 @@ char **getInput(int *n, int *lineSize){
 int *getWordSizes(char **vector, int n){
 	int i, *wordSizes = (int*) malloc (sizeof(int)*n);
 	for (i=0; i<n; i++){
-		wordSizes[i] = strlen(vector[i])-1;
+		wordSizes[i] = strlen(vector[i]);
 	}
 	return wordSizes;
 }
 
-void printVector(char **vector, int n){
-	int i;
-	for (i=0; i<n; i++){
-		printf ("%s", vector[i]);
-	}
-}
-
-int **allocateIntMatrix(int n){
-	int i, **M = (int**) malloc (sizeof(int*) * n);
-	for (i=0; i<n; i++){
-		M[i] = (int*) malloc (sizeof(int) * n);
-	}
-	return M;
-}
-
-void wordWrap(char **vector, int *wordSizes, int n, int lineSize){
-	int i, j;
-	//extraSpace[a][b] possui os espacos extra se palavras entre a e b
-	//forem colocadas na mesma linha
-	int **extraSpace = allocateIntMatrix(n+1);
-	printf ("Calcula extraSpace\n");
-	for (i=1; i<=n; i++){
-		//Colocando apenas a palavra[i], o espaco restante sera o tamanhoMaximo - tamanhoPalavra
-		extraSpace[i][i] = lineSize - wordSizes[i-1];
-		
-		for (j=i+1; j<=n; j++){
-			//Para cada palavra adicionada, vou subtraindo o espaco restante pelo
-			//tamanho dessa palavra, e tambem contando +1(espaco)
-			extraSpace[i][j] = extraSpace[i][j-1] - wordSizes[j-1] - 1;
-		}
-	}
-
-	//lineCost[a][b] representa o custo em colocar as palavras
-	//de 'a' a 'b' na mesma linha (Custo = espacos livres^2)
-	int **lineCost = allocateIntMatrix(n+1);
-	printf ("Calcula lineCost\n");
-	for (i=1; i<=n; i++){
-		for (j=i; j<=n; j++){
-			if (extraSpace[i][j]<0){
-				//Nesse caso, as palavras de i a j nao cabem
-				//recebe infinito para mostrar que nao pode existir essa combinacao
-				lineCost[i][j] = INT_MAX;
-			} else {
-				//Nesse caso, cabem as palavras de i a j, entao eu
-				//calculo o quadrado
-				lineCost[i][j] = extraSpace[i][j] * extraSpace[i][j];
-			}
-		}
-	}
+int *wordWrap(char **vector, int *wordSizes, int n, int lineSize){
+	int i, j, k;
 
 	//Calculo o custo:
 	//C[n] representa o custo minimo de colocar as palavras de 1 a n
 	int *c = (int*) malloc (sizeof(int) * (n+1));
-	printf ("Calcula custo minimo\n");
+	int *p = (int*) malloc (sizeof(int) * (n+1));
+
 	c[0] = 0;
 	for (j=1; j<=n; j++){
 		c[j] = INT_MAX;
 		for(i=1; i<=j; i++){
-			if (c[i-1] != INT_MAX && lineCost[i][j] != INT_MAX){
-				c[j] = MIN(c[j], lineCost[i][j]+c[i-1]);
-				printf ("Custo min[%d] = %d\n", j, c[j]);
+			if (c[i-1] != INT_MAX){
+
+				int extraSpace = lineSize - wordSizes[i-1];
+				for (k=i+1; k<=j; k++){
+					//Para cada palavra adicionada, vou subtraindo o espaco restante pelo
+					//tamanho dessa palavra, e tambem contando +1(espaco)
+					extraSpace -= wordSizes[k-1] + 1;
+
+					if (extraSpace<0) break;
+				}
+
+				if (extraSpace >= 0 && (extraSpace*extraSpace + c[i-1] < c[j])){
+					c[j] = extraSpace*extraSpace + c[i-1];
+					p[j] = i;
+				}
 			}
 		}
 	}
 
-	printf ("Custo minimo:\n");
-	printf ("C[%d] = %d\n", n, c[n]);
+	return p;
+}
+
+void printSolution (char **vector, int *p, int n){
+    int i;
+    if (p[n] != 1) printSolution (vector, p, p[n]-1);
+
+    for (i=p[n]; i<=n; i++){
+    	printf ("%s ", vector[i-1]);
+	}
+	printf ("\n");
 }
 
 int main(int argc, char **argv){
@@ -103,7 +81,7 @@ int main(int argc, char **argv){
 	char **vector = getInput(&n, &lineSize);
 	int *wordSizes = getWordSizes(vector, n);
 
-	wordWrap(vector, wordSizes, n, lineSize);
+	int *p = wordWrap(vector, wordSizes, n, lineSize);
 
-	printVector(vector, n);
+	printSolution(vector, p, n);
 }
